8ab75df402456b46154e535185fc9f9581acc13b
Roman Son <son.romario@gmail.com>
2024-06-06 10:35:17 +0300

    Add sections intro.vue component. Add home-sections.json with model for home intro sections.

---
+++ components/sections/intro.vue
@@ -0,0 +1,49 @@
+<template>
+    <section
+        :class="'intro-section ' + cssClass"
+        :style="{
+            'background-image': `url('/images/${background}')`,
+        }"
+    >
+        <div class="container">
+            <div class="nonna-container">
+                <WidgetsHeader v-if="withHeader"/>
+                <div class="section-content">
+                    <h1 v-html="title"></h1>
+                    <p v-html="text"></p>
+                    <nuxt-link v-if="button" :to="'/'" class="nonna-btn" aria-current="page">{{ button }}</nuxt-link>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+defineProps({
+    background: {
+        type: String,
+    },
+    title:{
+        type: String,
+    },
+    text: {
+        type: String,
+    },
+    button: {
+        type: String,
+        default: '',
+    },
+    withHeader: {
+        type: Boolean,
+        default: false,
+    },
+    cssClass: {
+        type: String,
+        default: '',
+    }
+});
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
-        <SectionsHomeSection1 />
+        <SectionsIntro
+            v-for="(section, index) in sections.data"
+            :key="index"
+            :title="section.title"
+            :text="section.text"
+            :button="section.button"
+            :background="section.background"
+            :with-header="index === 0"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
-
+import sections from '../data/home-sections.json'
 </script>
 
 <style scoped>
