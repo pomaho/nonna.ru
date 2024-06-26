11fe976e3c4a0800c7e78d99a1161fbbd41c4657
Roman Son <son.romario@gmail.com>
2024-06-26 19:34:20 +0300

    Fix change language styles and names

--- components/widgets/header.vue
+++ components/widgets/header.vue
@@ -2,20 +2,9 @@
     <div class="header-logo-and-menu">
         <MediaLogo/>
         <MenuMainMenu/>
-        <NuxtLink v-for="locale in availableLocales" :key="locale.code" :to="switchLocalePath(locale.code)">
-            {{ locale.name }}
-        </NuxtLink>
+        <WidgetsLanguage />
     </div>
 </template>
-
-<script setup>
-const { locale, locales } = useI18n()
-const switchLocalePath = useSwitchLocalePath()
-
-const availableLocales = computed(() => {
-    return locales.value.filter(i => i.code !== locale.value)
-})
-</script>
 
 <style scoped>
 
---
+++ components/widgets/language.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="language">
+        <NuxtLink class="language-link" v-for="locale in availableLocales" :key="locale.code" :to="switchLocalePath(locale.code)">
+            {{ locale.name.toUpperCase() }}
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+const { locale, locales } = useI18n()
+const switchLocalePath = useSwitchLocalePath()
+
+const availableLocales = computed(() => {
+    return locales.value.filter(i => i.code !== locale.value)
+});
+
+</script>
