2ba01c9615f34a3036e3757dd96cc95426ba100c
Roman Son <son.romario@gmail.com>
2024-07-30 19:34:16 +0300

    Fix responsive

--- pages/bespoke.vue
+++ pages/bespoke.vue
@@ -4,6 +4,7 @@
             :title="$t(`bespoke-intro-section-title`)"
             :text="$t(`bespoke-intro-section-text`)"
             :button="$t(`bespoke-intro-section-button`)"
+            :buttonLink="'about'"
             :background="`bespoke/section-1-bg.jpeg`"
             :with-header="true"
         />
--- pages/contacts.vue
+++ pages/contacts.vue
@@ -3,7 +3,6 @@
         <SectionsIntro
             :title="$t(`contacts-intro-section-title`)"
             :text="$t(`contacts-intro-section-text`)"
-            :button="$t(`contacts-intro-section-button`)"
             :background="`contacts/section-1-bg.jpeg`"
             :with-header="true"
         />
