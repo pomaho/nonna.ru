1f8fd3f48a4caae4eb2186bcedc21166dcc407c4
Roman Son <son.romario@gmail.com>
2024-08-15 00:57:20 +0300

    Add video BG for page about manufacture

--- components/media/logo.vue
+++ components/media/logo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="nonna-logo-container">
         <nuxt-link :to="localePath('/')" class="nav-link" aria-current="page">
             <img src="@/assets/images/nonna-new.png" alt="" />
         </nuxt-link>
--- components/sections/intro-video-bg.vue
+++ components/sections/intro-video-bg.vue
@@ -1,19 +1,17 @@
 <template>
     <section
-        :class="'intro-section ' + cssClass"
+        :class="'intro-section video-section' + cssClass"
     >
-        <video class="video-background"
-            src="/IMG_9095.M4V"
-            controls=""
-            poster="/images/about/video-poster.png" width="100%">
-            Sorry, your browser does not support HTML5 video.
-        </video>
-
-        <div class="shading"></div>
-        <div class="container">
-            <div class="nonna-container">
+        <div>
+            <div class="container">
                 <WidgetsHeader />
             </div>
+            <video class="video-background"
+                   src="/IMG_9095.M4V"
+                   controls=""
+                   poster="/images/about/video-poster.png" width="100%">
+                Sorry, your browser does not support HTML5 video.
+            </video>
         </div>
     </section>
 </template>
