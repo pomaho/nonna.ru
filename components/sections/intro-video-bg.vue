f462b7e7a31d686744feca4f501563b9b0f0bca1
Roman Son <son.romario@gmail.com>
2024-09-02 22:14:47 +0300

    Add webm urls

--- components/sections/intro-video-bg.vue
+++ components/sections/intro-video-bg.vue
@@ -8,6 +8,8 @@
                :src="`/${videoUrl}`"
                controls=""
                poster="/images/about/video-poster.png" width="100%">
+            <source :src="`/${videoUrlWebm}`" type="video/webm">
+            <source :src="`/${videoUrl}`" type="video/mp4">
             Sorry, your browser does not support HTML5 video.
         </video>
     </section>
@@ -22,6 +24,7 @@ defineProps({
 });
 const {locale} = useI18n();
 const videoUrl = locale.value === 'ru' ? 'IMG_0821_H.m4v' : 'IMG_0812.m4v';
+const videoUrlWebm = locale.value === 'ru' ? 'IMG_0821_H.webm' : 'IMG_0812.webm';
 
 </script>
 
