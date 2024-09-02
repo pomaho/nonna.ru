db17db7a862e63a775d769a2239df730578d9cba
Roman Son <son.romario@gmail.com>
2024-09-02 22:36:48 +0300

    Try ebonyandco video

--- components/sections/intro-video-bg.vue
+++ components/sections/intro-video-bg.vue
@@ -7,7 +7,7 @@
                preload="none"
                controls=""
                poster="/images/about/video-poster.png" width="100%">
-            <source :src="`/${videoUrlWebm}`" type="video/webm">
+<!--            <source :src="`/${videoUrlWebm}`" type="video/webm">-->
             <source :src="`/${videoUrl}`" type="video/mp4">
             Sorry, your browser does not support HTML5 video.
         </video>
@@ -22,7 +22,7 @@ defineProps({
     }
 });
 const {locale} = useI18n();
-const videoUrl = locale.value === 'ru' ? 'IMG_0821_H.m4v' : 'IMG_0812.m4v';
+const videoUrl = locale.value === 'ru' ? 'ebanyandco.mp4' : 'IMG_0812.m4v';
 const videoUrlWebm = locale.value === 'ru' ? 'IMG_0821_H.webm' : 'IMG_0812.webm';
 
 </script>
